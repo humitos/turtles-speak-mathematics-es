cb02c3f911e753b5ba8e07a815cc5909d47fbfe1
Manuel Kaufmann <humitos@gmail.com>
2012-09-08 13:27:46 -0300

    Revert "Initial PDF version configurations"
    This reverts commit 87edce4d5c5103fc5752f907332985782f69ffe8.
    
    This was an accidental commit on this branch

--- make_pdf.sh
+++
@@ -1,1 +0,0 @@
-rst2pdf docs/index.rst --extension=inkscape --output=turtles-speak-mathematics-es__unfinished.pdf --default-dpi=62 --stylesheets=docs/_styles/my.style --break-level=1 --language=es_AR --smart-quotes=1 -v
